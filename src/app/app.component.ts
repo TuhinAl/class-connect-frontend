1f2197fe154f5d7090878f883110cc1fbb7d377d
tuhinal <alauddintuhin1411@gmail.com>
2025-02-01 19:36:00 +0600

    added dynamic year in footer, added new design for header home, login, register and added router for them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Coaching Center Management System';
   userName = 'Admin';
   logout() {
     this.userName = 'Super Admin';
   }
+
+  currentYear: number;
+  
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
+  ngOnInit(): void {}
 }
